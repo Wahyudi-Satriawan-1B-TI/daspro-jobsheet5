107e3ca20d5b39f02d93ebf0116d98cdad55e9d8
Wahyudi-Satriawan-1B-TI <wahyudisatriawanh@gmail.com>
2024-09-25 07:57:30 +0700

    penyelesaian code pemilihan hari

---
+++ PemilihanHari25.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class PemilihanHari25 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+// Deklarasi variabel
+String dayName, dayType;
+
+System.out.print("Input day name: ");
+dayName = sc.nextLine();
+
+switch (dayName.toLowerCase()) {
+    case "monday":
+    case "tuesday":
+    case "wednesday":
+    case "thursday":
+    case "friday":
+        dayType = "weekday";
+        break;
+    case "saturday":
+    case "sunday":
+        dayType = "weekend";
+        break;
+    default:
+        dayType = "invalid day name";
+        break;
+}
+
+System.out.println(dayName + " is a " + dayType);
+
+    }
+}
